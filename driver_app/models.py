cacaeafa3f96bd7e0797a1bb63a0bdade9769345
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2023-11-02 17:23:52 -0600

    Migrated database, added driverEntry model

--- driver_app/models.py
+++ driver_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class TripEntry(models.Model):
+
+
+    driverName = models.CharField("Driver Name", max_length=200)
+    date = models.CharField("Date", max_length=200)
+    driverAddress = models.CharField("Driver Address", max_length=200)
+    vin = models.CharField("VIN #", max_length=17)
+    managerName = models.CharField("Manager Name", max_length=200)
+    stockNum = models.CharField("Stock Number", max_length=200)
+    codriver = models.CharField("Codriver", max_length=200. blank - True)
+    milesDriven = models.CharField("Miles Driven", max_length=200)
+    description = models.TextField(blank = True)
+
